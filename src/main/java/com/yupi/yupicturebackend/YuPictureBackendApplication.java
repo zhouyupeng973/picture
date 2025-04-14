93a187d341a9302354b21af7db24c118fb31d8c6
hp <2291394261@qq.com>
2025-04-14 20:53:51 +0800

    1

---
+++ src/main/java/com/yupi/yupicturebackend/YuPictureBackendApplication.java
@@ -0,0 +1,16 @@
+package com.yupi.yupicturebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.mybatis.spring.annotation.MapperScan;
+
+@SpringBootApplication
+@MapperScan("com.yupi.yupicturebackend.mapper")
+public class YuPictureBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YuPictureBackendApplication.class, args);
+    }
+
+}
+
